fdac112a8e6df77853f147076bac8d0d24f784ef
tiagocouto22 <161645126+tiagocouto22@users.noreply.github.com>
2025-06-13 23:42:34 +0100

    Update wave.rs

--- src/methods/guest/src/bin/wave.rs
+++ src/methods/guest/src/bin/wave.rs
@@ -1,16 +1,25 @@
 use fleetcore::{BaseInputs, BaseJournal};
 use risc0_zkvm::guest::env;
-//use risc0_zkvm::Digest;
-//use sha2::{Digest as _, Sha256};
+use risc0_zkvm::Digest;
+use sha2::{Digest as ShaDigestTrait, Sha256};
 
 fn main() {
+    // Lê os inputs
+    let input: BaseInputs = env::read();
 
-    // read the input
-    let _input: BaseInputs = env::read();
+    // Calcula o hash da board + random
+    let mut hasher = Sha256::new();
+    hasher.update(&input.board);
+    hasher.update(input.random.as_bytes());
+    let hash = hasher.finalize();
+    let board_digest = Digest::try_from(hash.as_slice()).unwrap();
 
-    // TODO: do something with the input
-    let output= BaseJournal::default();
+    // Preenche o journal
+    let output = BaseJournal {
+        gameid: input.gameid,
+        fleet: input.fleet,
+        board: board_digest,
+    };
 
-    // write public output to the journal
     env::commit(&output);
 }
